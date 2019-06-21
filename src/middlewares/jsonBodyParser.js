8ebd84319ece2aa1afab9de16c6ecc71545deb11
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2019-06-21 11:43:21 +0100

    Migigate prototype poisoning by parsing JSON with bourne

--- src/middlewares/jsonBodyParser.js
+++ src/middlewares/jsonBodyParser.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors')
 const contentTypeLib = require('content-type')
+const bourne = require('@hapi/bourne')
 
 module.exports = (opts) => ({
   before: (handler, next) => {
@@ -13,7 +14,7 @@ module.exports = (opts) => ({
       const { type } = contentTypeLib.parse(contentType)
       if (type === 'application/json') {
         try {
-          handler.event.body = JSON.parse(handler.event.body, opts.reviver)
+          handler.event.body = bourne.parse(handler.event.body, opts.reviver)
         } catch (err) {
           throw new createError.UnprocessableEntity('Content type defined as JSON but an invalid JSON was provided')
         }
